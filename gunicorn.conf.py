648fb68479c13e6e005a119f16c897205958b9db
Arjun Chavan <arjun.chavan@pens.com>
2023-04-04 02:50:54 +0530

    Final gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,13 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 LOCAL_IP = os.getenv('LOCAL_IP')
-KEY_FILE = os.getenv('KEY_FILE')
-CERT_FILE = os.getenv('CERT_FILE')
-CHAIN_FILE = os.getenv('CHAIN_FILE')
 
 bind = LOCAL_IP
-workers = 1
-
-keyfile = KEY_FILE
-certfile = CERT_FILE
-ca_certs = CHAIN_FILE
+workers = 10
